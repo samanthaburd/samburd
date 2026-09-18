05a01e25e39240a15602952647ef39295dc98d4e
“Samantha <samanthaburd@berkeley.edu>
2026-09-18 10:50:12 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,71 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+# Git is the program that runs on my computer and tracks changes to files, whether that's who changed it, when, or what. It allows me to go back to earlier versions. It also works locally and does not rely on the internet. GitHub is a website that hosts Git repositories online. Here, you can store and share projects. Git bash is a terminal application, or a way to run Git commands, on windows.
+ 
+# 2) What’s the difference between the terminal and the command line?
+#The terminal is the program or application where I can access to command line. The command line is the method of communicating with my computer, where I am able to directly type text commands.
+
+# 3) How does Windows PowerShell differ from Git Bash?
+#Powershell is Microsoft's shell, built for windows. It's intended to be Window's native control. Git Bash is unix-style and has been packaged to run on windows so that people can use Git without needing to know Window's Powershell's specific syntax.
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+#Python is the actual coding language. It's what we write code in, which has it's own rules and syntax. Conda is a package/environment manager, which serves to install packages and manage environments. Anaconda is a bundle which includes python, Conda, and many packages that are commonly used. This means that instead of individually downloading a bunch of packages, you get a lot of commonly used ones all at once.
+
+# 5) What is VS Code? 
+#VS code is a code editor created by Microsoft, where you are able to wite and edit code.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+#Jupyter is a tool where you are able to write and run code in cells, so you can execute bit by bit. (I've used it in classes like E7.) Jupyter Lab is a much more extensive workspace similar to an IDE, where you can have multiple notebooks, files, etc open side by side.
+
+# 7) What does ~/ mean?
+#the tilde (~) indicates your home directory
+
+# 8) What’s the difference between an absolute path and a relative path?
+# An absolute path is the full path/address to a file/folder, starting from the root. (it does not depend on where you are). The relative path is the address in relation to your current location.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+#absolute: main/Users/samanthaburd/Astro-98/course_assignments/homework2          relative: Astro-98/course_assigments/homework2/
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+#cd ..
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+#remove my current file/folder
+
+# 12) What do the following commands do?
+# git add
+#adds changed files to current folder, staging changes
+
+# git commit
+#saves a snapshot of the staged changes to history
+
+# git push
+#uploads commits to a repository
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+# git add . will stage everything inside the current folder, while git add <file> will stage only the file called
+
+# 14) What do "git status" and "git log -1" do?
+#git status shows the current state of what im working on, whether my files are staged, not staged, or not be tracked by git. Git log -1 shows the history of my most recent commit.
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+#cloning directly copies an entire repository for the first time. Once you have a local copy, it will update my local copy.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+#my most frustrating error was when I was trying to upload my first homework, which was simply due to a misnaming of files. I fixed it by creating a new file and copying and pasting my code.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+#I don't understand how to rename a file.
+
+# 18) Tell me a fun fact!
+#I want to get my pilots license!
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print((5*2)//3)
+#this multiplies 5 by 2 and performs floor division by 3, giving us 3.
